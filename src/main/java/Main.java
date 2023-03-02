e0704babf947b78601c065fef898bc72abc83994
William Torres <wiliam.wgtm@gmail.com>
2023-03-01 21:12:11 -0300

    instanciando a classe ave

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
+package src.main.java;
+
+import src.main.java.animals.Ave;
+
 public class Main {
 
     public static void main(String[] args) {
+        Ave tucano = new Ave();
+        tucano.setBico("bico fino");
+        tucano.setCor("vermelho");
+        tucano.setEspecie("tucano");
+        System.out.println(tucano.getBico());
+        System.out.println(tucano.getCor());
+        System.out.println(tucano.getEspecie());
 
+        Ave papagaio = new Ave();
+        papagaio.setBico("bico longo");
+        papagaio.setCor("azul");
+        papagaio.setEspecie("arara");
+        System.out.println(papagaio.getBico());
+        System.out.println(papagaio.getCor());
+        System.out.println(papagaio.getEspecie());
     }
 }
--- src/main/java/animals/Ave.java
+++ src/main/java/animals/Ave.java
@@ -1,4 +1,31 @@
-package animals;
+package src.main.java.animals;
 
 public class Ave {
+    private String cor;
+    private String bico;
+    private String especie;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getBico() {
+        return bico;
+    }
+
+    public void setBico(String bico) {
+        this.bico = bico;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
 }
